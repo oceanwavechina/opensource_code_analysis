2aed732094ca79a0293965e27e721a3f78e5bc9b
liuyanan <>
2021-08-25 17:56:46 +0800

    完善了程序启动的分析

---
+++ linux-4.19.180/resources/exec_test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int g_a = 1;
+
+int main() {
+    printf("hello, world!\n");
+    return 0;
+}
---
+++ linux-4.19.180/resources/exec_test.s
@@ -0,0 +1,35 @@
+gcc -S test.c
+cat test.s
+	.file	"test.c"
+.globl g_a
+	.data
+	.align 4
+	.type	g_a, @object
+	.size	g_a, 4
+g_a:
+	.long	1
+	.section	.rodata
+.LC0:
+	.string	"hello, world!"
+	.text
+.globl main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC0, %edi
+	call	puts
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.4.7 20120313 (Red Hat 4.4.7-16)"
+	.section	.note.GNU-stack,"",@progbits
